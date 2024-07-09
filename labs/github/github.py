b5905ae5fb23fa722c966b28f42458b22dacc892
lmoreira-runtime <165663989+lmoreira-runtime@users.noreply.github.com>
2024-07-09 09:00:14 +0100

    GitHub integration (#13)
    * setup project
    
    * Update environment.yml
    
    * github
    
    * github
    
    * logs + config + tests + makefile
    
    * class Github
    
    * Changes to be committed:
    	modified:   .gitignore
    	modified:   README.md
    	deleted:    docs/README.md
    	deleted:    docs/docs/getting-started.md
    	deleted:    docs/docs/index.md
    	deleted:    docs/mkdocs.yml
    	modified:   environment.yml
    	deleted:    labs/.gitignore
    	modified:   labs/github/__init__.py
    	modified:   labs/github/github.py
    	modified:   labs/test/test_github.py
    	new file:   labs/test/vcr_cassettes/github_issue_invalid_token.yml
    	new file:   labs/test/vcr_cassettes/github_issues_invalid_token.yml
    	new file:   labs/test/vcr_cassettes/issue.yaml
    	new file:   labs/test/vcr_cassettes/issue_not_found.yaml
    	new file:   labs/test/vcr_cassettes/issues.yaml
    	new file:   labs/test/vcr_cassettes/issues_from_all_assignees.yaml
    	new file:   labs/test/vcr_cassettes/issues_with_state.yaml
    	new file:   pixi.lock
    	modified:   pyproject.toml

---
+++ labs/github/github.py
@@ -0,0 +1,73 @@
+import requests
+from dataclasses import dataclass
+from labs.config import GITHUB_ACCESS_TOKEN
+from labs.config import GITHUB_API_BASE_URL
+from labs.config import GITHUB_OWNER
+from labs.config import GITHUB_REPO
+from labs.config import GITHUB_USERNAME
+from labs.config import get_logger
+
+@dataclass
+class GithubRequests:
+    """Class to handle Github API requests"""
+    access_token = GITHUB_ACCESS_TOKEN
+    api_base_url = GITHUB_API_BASE_URL
+    owner = GITHUB_OWNER
+    repo = GITHUB_REPO
+    username = GITHUB_USERNAME
+    logger = get_logger(__name__)
+
+    def issues(self, assignee=None, state='open', per_page=100):
+        if assignee is None:
+            assignee = self.username
+        try:
+            url = f'{self.api_base_url}/repos/{self.owner}/{self.repo}/issues'
+            headers = {
+                'Authorization': f'token {self.access_token}',
+                'Accept': 'application/vnd.github.v3+json',
+            }
+            params = {
+                'state': state,
+                'per_page': per_page,
+            }
+            if assignee != 'all':
+                params['assignee'] = assignee
+
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            response_json = response.json()
+            self.logger.debug(str(response_json))
+            return response_json
+
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"HTTP Request failed: {e}")
+        except KeyError as e:
+            self.logger.error(f"Missing key in access data: {e}")
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+        return []
+
+    def issue(self, issue_id):
+        try:
+            url = f'{self.api_base_url}/repos/{self.owner}/{self.repo}/issues/{issue_id}'
+            headers = {
+                'Authorization': f'token {self.access_token}',
+                'Accept': 'application/vnd.github.v3+json',
+            }
+
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            response_json = response.json()
+            self.logger.debug(str(response_json))
+            return response_json
+
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"HTTP Request failed: {e}")
+            return None
+        except KeyError as e:
+            self.logger.error(f"Missing key in access data: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+            return None
+
