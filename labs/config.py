f60f8ebe94e2a93018caa65364790e0468a39c39
FilipaSdS <101112271+FilipaSdS@users.noreply.github.com>
2024-07-12 15:03:33 +0100

    28 GitHub additional actions (#30)
    * add addional git actions
    
    * commit correction
    
    * test fixing
    
    * remove notebook

--- labs/config.py
+++ labs/config.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -6,7 +5,7 @@ import logging
 from pythonjsonlogger import jsonlogger
 
 # Load environment variables from .env file if it exists
-load_dotenv()
+load_dotenv(dotenv_path="labs/.env")
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
@@ -32,12 +31,12 @@ MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
-GITHUB_ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
-GITHUB_OWNER = os.environ['GITHUB_OWNER']
-GITHUB_REPO = os.environ['GITHUB_REPO']
-GITHUB_USERNAME = os.environ['GITHUB_USERNAME']
-GITHUB_API_BASE_URL = 'https://api.github.com'
+GITHUB_ACCESS_TOKEN = os.environ["GITHUB_ACCESS_TOKEN"]
+GITHUB_OWNER = os.environ["GITHUB_OWNER"]
+GITHUB_REPO = os.environ["GITHUB_REPO"]
+GITHUB_USERNAME = os.environ["GITHUB_USERNAME"]
+GITHUB_API_BASE_URL = "https://api.github.com"
+
 
 def get_logger(module_name):
     return logging.getLogger(module_name)
-
