c48b4bf13d4aeff5cb1efebec98f5b77030396a4
Carlos Martinez <ctmartinez1992@gmail.com>
2024-07-12 15:22:37 +0100

    20 - LiteLLM (#32)
    Co-authored-by: Carlos Martinez <c.martinez@runtime-revolution.com>

---
+++ litellm_service/request.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+class RequestLiteLLM:
+    def __init__(self):
+        load_dotenv("litellm_service/.env")
+
+        self.api_key = os.environ.get("LITELLM_API_KEY", None)
+        if not self.api_key:
+            raise Exception("LITELLM_API_KEY missing.")
+
+    def completion(self, messages, model="llm-model"):
+        headers = {
+            "Accept": "application/json",
+            "API-Key": self.api_key,
+        }
+
+        data = {"messages": [{"role": "user", "content": message} for message in messages]}
+        result = requests.post(
+            f"http://0.0.0.0:4000/chat/completions?model={model}",
+            headers=headers,
+            json=data,
+        )
+
+        return result.json()["choices"][0]["message"]["content"]
+
+
+# print(RequestLiteLLM().completion(messages=["Say hello."]))
