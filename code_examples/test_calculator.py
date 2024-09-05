61aed9aefe2800ff47d043286847349d12013181
FilipaSdS <101112271+FilipaSdS@users.noreply.github.com>
2024-09-05 21:29:00 +0100

    fix

--- code_examples/test_calculator.py
+++ code_examples/test_calculator.py
@@ -1,7 +1,6 @@
-
 import unittest
 
-from calculator import Calculator
+from code_examples.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
@@ -33,29 +32,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(5, 2), 10)
 
-    def test_multiply_negative_with_positive(self):
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
-
-    def test_multiply_negative_with_negative(self):
-        self.assertEqual(self.calc.multiply(-3, -4), 12)
-
-    def test_multiply_zeros(self):
-        self.assertEqual(self.calc.multiply(0, 5), 0)
+    def test_multiply_by_zero(self):
+        self.assertEqual(self.calc.multiply(5, 0), 0)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 5), 2.0)
-
-    def test_divide_negative_with_positive(self):
-        self.assertEqual(self.calc.divide(-3, 1), -3.0)
-
-    def test_divide_negative_with_negative(self):
-        self.assertEqual(self.calc.divide(-6, -3), 2.0)
+        self.assertEqual(self.calc.divide(10, 2), 5)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ValueError):
             self.calc.divide(10, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
