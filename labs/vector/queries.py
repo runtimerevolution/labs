5cbab1d288c5ba7b606e326870de5dd4523e6a01
João Gomes <j.gomes@runtime-revolution.com>
2024-09-19 15:15:57 +0100

    imporved pytest conftest

--- labs/vector/queries.py
+++ labs/vector/queries.py
@@ -1,19 +1,7 @@
 import psycopg2 as psycopg
-from sqlalchemy import Text, text
+from sqlalchemy import text
 
 from labs.vector.connect import create_db_connection
-
-
-create_extension_sql = "CREATE EXTENSION IF NOT EXISTS vector;"
-create_table_embeddings_sql = """
-CREATE TABLE IF NOT EXISTS embeddings (
-  id SERIAL PRIMARY KEY,
-  embedding vector,
-  file_and_path text,
-  text text,
-  created_at timestamptz DEFAULT now()
-);
-"""
 
 
 def db_connector():
@@ -40,14 +28,6 @@ def db_connector():
     return decorator
 
 
-@db_connector()
-def setup_db(connection, cursor):
-    cursor.execute(create_extension_sql)
-    cursor.execute(create_table_embeddings_sql)
-    connection.commit()
-    return True
-
-
 def select_embeddings(session):
     return session.execute(text("SELECT * FROM embeddings;")).fetchall()
 
@@ -57,10 +37,10 @@ def reembed_code(connection, cursor, files_and_texts, embeddings):
     cursor.execute("DELETE FROM embeddings;")
     connection.commit()
 
-    for text, embedding_obj in zip(files_and_texts, embeddings.data):
+    for gen_text, embedding_obj in zip(files_and_texts, embeddings.data):
         cursor.execute(
             "INSERT INTO embeddings (embedding, file_and_path, text) VALUES (%s, %s, %s)",
-            (embedding_obj["embedding"], text[0], text[1]),
+            (embedding_obj["embedding"], gen_text[0], gen_text[1]),
         )
     connection.commit()
     return True
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,64 +4,70 @@ import pytest
 from labs.config import settings
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
 from labs.rag.embeddings import Embedding
 from labs.vector.connect import create_db_connection
 
 
-def is_postgres_available():
-    connection = None
-    cursor = None
-    try:
-        connection = create_db_connection()
-        if not connection:
-            return False
+# def is_postgres_available():
+#     connection = None
+#     cursor = None
+#     try:
+#         connection = create_db_connection()
+#         if not connection:
+#             return False
 
-        cursor = connection.cursor()
-        cursor.execute("SELECT 1")
-        return True
-    except Exception as ex:
-        return False
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-
-
-@pytest.fixture(scope="session")
-def database():
-    yield psycopg.connect(
-        database=settings.DATABASE_NAME,
-        user=settings.DATABASE_USER,
-        password=settings.DATABASE_PASS,
-        host=settings.DATABASE_HOST,
-        port=settings.DATABASE_PORT,
-    )
+#         cursor = connection.cursor()
+#         cursor.execute("SELECT 1")
+#         return True
+#     except Exception as ex:
+#         return False
+#     finally:
+#         if cursor:
+#             cursor.close()
+#         if connection:
+#             connection.close()
 
 
-@pytest.fixture(scope="session")
-def db_engine(request):
-    """yields a SQLAlchemy engine which is suppressed after the test session"""
-    engine_ = create_engine(settings.DATABASE_URL, echo=True)
-    yield engine_
-    engine_.dispose()
+# @pytest.fixture(scope="session")
+# def database():
+#     yield psycopg.connect(
+#         database=settings.DATABASE_NAME,
+#         user=settings.DATABASE_USER,
+#         password=settings.DATABASE_PASS,
+#         host=settings.DATABASE_HOST,
+#         port=settings.DATABASE_PORT,
+#     )
 
 
-@pytest.fixture(scope="session")
-def db_session_factory(db_engine):
-    """returns a SQLAlchemy scoped session factory"""
-    return scoped_session(sessionmaker(bind=db_engine))
+# @pytest.fixture(scope="session")
+# def db_engine(request):
+#     """yields a SQLAlchemy engine which is suppressed after the test session"""
+#     engine_ = create_engine(settings.DATABASE_URL, echo=True)
+#     yield engine_
+#     engine_.dispose()
+
+
+# @pytest.fixture(scope="session")
+# def db_session_factory(db_engine):
+#     """returns a SQLAlchemy scoped session factory"""
+#     return scoped_session(sessionmaker(bind=db_engine))
+
+
+engine = create_engine(settings.DATABASE_URL, echo=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 
 @pytest.fixture(scope="function")
-def db_session(db_session_factory):
+def db_session():
     """yields a SQLAlchemy connection which is rollbacked after the test"""
-    session_ = db_session_factory()
-    yield session_
-    session_.rollback()
-    session_.close()
+    Base.metadata.create_all(engine)
+    session = Session()
+    yield session
+    session.rollback()
+    session.close()
 
 
 @pytest.fixture(scope="module")
