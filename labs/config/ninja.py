0236d02ff5346221dba6f1b0f053c331cedf362d
João Verdasca <jfrverdasca@ua.pt>
2024-11-12 17:49:31 +0000

    Fix: Changed API name in docs

--- labs/config/ninja.py
+++ labs/config/ninja.py
@@ -2,7 +2,7 @@ from api.codemonkey_endpoints import router as codemonkey_router
 from api.github_endpoints import router as github_router
 from ninja import NinjaAPI
 
-ninja_api = NinjaAPI()
+ninja_api = NinjaAPI(title="Labs")
 
 ninja_api.add_router("", codemonkey_router)
 ninja_api.add_router("", github_router)
