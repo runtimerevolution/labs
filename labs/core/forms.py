1d383bd5cdacf98a0a7b73dc7ecdc2c35907f571
Andre Carvalho <andre.cajus.carvalho@gmail.com>
2025-01-31 14:49:33 +0000

    corrected behavior of select models in admin

--- labs/core/forms.py
+++ labs/core/forms.py
@@ -1,11 +1,13 @@
 import os
 
-from core.models import Project
-from django import forms
+from core.models import Project, ModelTypeEnum
+from django.forms import (
+    ModelForm, BaseModelFormSet, ValidationError, ChoiceField
+)
 from django.conf import settings
 
 
-class ProjectForm(forms.ModelForm):
+class ProjectForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.get_projects_directories()
@@ -17,7 +19,7 @@ class ProjectForm(forms.ModelForm):
             if os.path.isdir(full_directory_path):
                 directories.append((full_directory_path, directory))
 
-        self.fields["path"] = forms.ChoiceField(
+        self.fields["path"] = ChoiceField(
             choices=directories,
             label="Project directory",
             required=True,
@@ -26,3 +28,43 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ["name", "description", "path", "url"]
+
+
+class ModelFormSet(BaseModelFormSet):
+    def clean(self):
+        super().clean()
+
+        embedding_count = 0
+        llm_count = 0
+        errors = []
+
+        embedding_name = ModelTypeEnum.EMBEDDING.name
+        llm_name = ModelTypeEnum.LLM.name
+
+        for form in self.forms:
+            if not form.cleaned_data or form.cleaned_data.get("DELETE", False):
+                continue
+
+            model_type = form.cleaned_data["model_type"]
+            active = form.cleaned_data["active"]
+
+            if model_type == embedding_name and active:
+                embedding_count += 1
+            elif model_type == llm_name and active:
+                llm_count += 1
+
+        if embedding_count != 1:
+            errors.append(
+                ValidationError(
+                    f"You must have exactly 1 active {embedding_name}, found {embedding_count}."
+                )
+            )
+        if llm_count != 1:
+            errors.append(
+                ValidationError(
+                    f"You must have exactly 1 active {llm_name}, found {llm_count}."
+                )
+            )
+
+        if errors:
+            raise ValidationError(errors)
