0bd761bdaa454d06ec748add1f3091efc5fee58f
JdFSilva <j.silva@runtime-revolution.com>
2025-02-04 15:19:07 +0000

    [#201] Factories (#206)
    * Feat: Add model Project
    
    * Feat: Fix tests
    
    * Fix: Removed unused import
    
    * Fix variables factory
    
    * Use ProviderEnum in variable factory
    
    * Fix model factory
    
    * Fix project factory
    
    * Fix vectorizer factory
    
    * Fix prompt factory
    
    * Remove unused variables
    
    * Add factory for embedding model
    
    * Remove factory test
    
    ---------
    
    Co-authored-by: João Verdasca <jfrverdasca@ua.pt>

---
+++ labs/core/factories.py
@@ -0,0 +1,93 @@
+import factory
+from factory.django import DjangoModelFactory
+
+from .models import (
+    Model,
+    ModelTypeEnum,
+    Project,
+    Prompt,
+    ProviderEnum,
+    Variable,
+    VectorizerEnum,
+    VectorizerModel,
+    WorkflowResult,
+)
+
+VARIABLES_NAMES = ["OPENAI_API_KEY", "DEFAULT_VECTORIZER", "DEFAULT_PERSONA", "DEFAULT_INSTRUCTION"]
+
+
+class VariableFactory(DjangoModelFactory):
+    class Meta:
+        model = Variable
+
+    provider = factory.Iterator([provider.name for provider in ProviderEnum if provider != ProviderEnum.NO_PROVIDER])
+    name = factory.Iterator(VARIABLES_NAMES)
+    value = factory.Faker("text")
+
+    @factory.post_generation
+    def default_vectorizer_value_validation(self, create, extracted, **kwargs):
+        if self.name == "DEFAULT_VECTORIZER" and self.value not in ["CHUNK_VECTORIZER", "PYTHON_VECTORIZER"]:
+            raise ValueError("Invalid vectorizer value")
+
+
+class ModelFactory(DjangoModelFactory):
+    class Meta:
+        model = Model
+
+    model_type = factory.Iterator([mt.name for mt in ModelTypeEnum])
+    provider = factory.Iterator([provider.name for provider in ProviderEnum if provider != ProviderEnum.NO_PROVIDER])
+    model_name = factory.Faker("word")
+    active = factory.Faker("boolean")
+
+
+class ProjectFactory(DjangoModelFactory):
+    class Meta:
+        model = Project
+
+    name = factory.Faker("word")
+    description = factory.Faker("text")
+    path = factory.Faker("file_path")
+    url = factory.Faker("url")
+
+
+class VectorizerFactory(DjangoModelFactory):
+    class Meta:
+        model = VectorizerModel
+
+    project = factory.SubFactory(ProjectFactory)
+    vectorizer_type = factory.Iterator([vectorizer.name for vectorizer in VectorizerEnum])
+
+    @factory.post_generation
+    def set_vectorizer_type(self, create, extracted, **kwargs):
+        if not self.vectorizer_type:
+            self.vectorizer_type = "CHUNK_VECTORIZER"
+
+
+class WorkflowResultFactory(DjangoModelFactory):
+    class Meta:
+        model = WorkflowResult
+
+    project = factory.SubFactory(ProjectFactory)
+    task_id = factory.Faker("uuid")
+    embed_model = factory.Faker("word")
+    prompt_model = factory.Faker("word")
+    embeddings = factory.Faker("text")
+    context = factory.Faker("text")
+    llm_response = factory.Faker("text")
+    modified_files = factory.Faker("text")
+
+
+class PromptFactory(DjangoModelFactory):
+    class Meta:
+        model = Prompt
+
+    project = factory.SubFactory(ProjectFactory)
+    persona = factory.Faker("text")
+    instruction = factory.Faker("text")
+
+    @factory.post_generation
+    def set_persona_and_instruction(self, create, extracted, **kwargs):
+        if not self.persona:
+            self.persona = "Default persona value"
+        if not self.instruction:
+            self.instruction = "Default instruction value"
