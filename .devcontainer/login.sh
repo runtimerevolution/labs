df1cd3ce94709b1c845c64dda38c883e3887d5e3
JdFSilva <j.silva@runtime-revolution.com>
2025-02-19 11:14:52 +0000

    Fix dev container (#222)
    * Remove .env.local from .dockerignore
    
    * Switch .env.local to .env
    
    * Switch .env.local to .env on docker-compose.yml
    
    * Add logs to login.sh
    
    * Fix dev container volume
    
    * Update Readme

--- .devcontainer/login.sh
+++ .devcontainer/login.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-source .env.local
-
 # Check if infisical credentials are present in the env
 if [ -z "$INFISICAL_CLIENT_ID" ] || [ -z "$INFISICAL_CLIENT_SECRET" ]; then
   echo "Infisical credentials not set in environment variables."
@@ -9,7 +7,11 @@ if [ -z "$INFISICAL_CLIENT_ID" ] || [ -z "$INFISICAL_CLIENT_SECRET" ]; then
 fi
 
 # Login to infisical
+echo "Getting Infisical Token"
 export INFISICAL_TOKEN=$(infisical login --domain=$INFISICAL_DOMAIN --method=universal-auth --client-id=$INFISICAL_CLIENT_ID --client-secret=$INFISICAL_CLIENT_SECRET --silent --plain)
+echo "Done"
 
 # Export secrets to zsh
+echo "Exporting Infisical Token"
 infisical secrets --domain=$INFISICAL_DOMAIN --projectId=$INFISICAL_PROJECT_ID --env=dev --plain | while IFS= read -r line; do echo "export $line" >> $HOME/.zshrc; done
+echo "Done"
